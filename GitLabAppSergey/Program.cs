017e5efb6fd886fbd9302fc53e1e7996e77f7c24
Сергей Сергей <striga1@mail.ru>
2020-05-30 13:49:54 +0300

    Добавьте файлы проекта.

---
+++ GitLabAppSergey/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabAppSergey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение параметра x:");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = Math.Sin(x) - 2 * Math.Cos(x);
+            Console.WriteLine($"Значение F = {F}.");
+            Console.ReadKey();
+
+        }
+    }
+}
